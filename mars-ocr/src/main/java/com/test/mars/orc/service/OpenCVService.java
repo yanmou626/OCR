dd7a83035f313bbbea8a2981092c9027fae86376
whp <wanghuapei@huan.tv>
2020-12-15 00:43:21 +0800

    delete big data

--- mars-ocr/src/main/java/com/test/mars/orc/service/OpenCVService.java
+++
@@ -1,7 +0,0 @@
-package com.test.mars.orc.service;
-
-public interface OpenCVService {
-    String correct(String base64,String correctPath);
-    String getTmpPath();
-    Boolean getSaveTmpImg();
-}
